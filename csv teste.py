8f60770152d2c474ad95592e8f17d488c859e601
marianafpessoa <marianafpessoa1995@gmail.com>
2022-11-24 12:47:08 -0300

    AtividadeSoftex

--- csv teste.py
+++ csv teste.py
@@ -1,13 +1,22 @@
-import pandas 
+import pandas as pd
 
-df = pandas.read_csv("notas_alunos.csv")
+alunos = pd.read_csv('notas_alunos.csv')
 
-media = (df["nota_1"] + df["nota_2"]) / 2
+media = (alunos['nota_1'] + alunos['nota_2']) / 2
+alunos['Media'] = media
 
-if(media >= 7):
+alunos.loc[alunos['Media'] >= 7, 'Situação'] = 'Aprovado'
+alunos.loc[alunos['faltas'] > 5, 'Situação'] = 'Reprovado'
+alunos.loc[alunos['Media'] < 7, 'Situação'] = 'Reprovado'
 
-   print("Aprovado")
+alunos.to_csv('alunos_situacao.csv') 
 
-elif(media < 7):
 
-   print("Reprovado")
+faltas = alunos['faltas'].max()
+print("O aluno que possui maior número de faltas é:\n"+str(faltas))
+
+mx = alunos['Media'].median()
+print("A media geral foi de:\n"+str(mx))
+
+mats = alunos['Media'].max()
+print("O aluno com a maior média foi:\n"+str(mats))
